20afd7a9b0ef34a7ce96649e6866132f591cf3e4
Dawen <darwinyang0805@gmail.com>
2026-06-26 07:20:22 +1000

    Launch Wezterm with max Windows

--- dot_config/wezterm/events/gui-startup.lua
+++ dot_config/wezterm/events/gui-startup.lua
@@ -2,5 +2,5 @@ local wt = require "wezterm"
 
 wt.on("gui-startup", function(cmd)
   local _, _, window = wt.mux.spawn_window(cmd or {})
-  window:gui_window():toggle_fullscreen()
+  window:gui_window():maximize()
 end)
